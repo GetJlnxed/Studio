9c7885a358bfc5b6d668e73d81d6ab04ac666967
getJinxed <mdm77728@gmail.com>
2020-02-28 13:58:50 +0300

    animate section headings

--- app/js/raw/main.js
+++ app/js/raw/main.js
@@ -1,12 +1,22 @@
-$(document).ready(function() {
-	$('#fullpage').fullpage({
-		autoScrolling:true,
-    anchors:['welcome', 'studio', 'process', 'forthPage', 'fifthPage'],
+$(document).ready(function () {
+  $('#fullpage').fullpage({
+    autoScrolling: true,
+    anchors: ['welcome', 'studio', 'process', 'forthPage', 'fifthPage'],
     menu: '#menu',
     css3: true,
-    verticalCentered: false
-	});
+    verticalCentered: false,
+    afterLoad: function (origin, destination, direction) {
+      if (this[0].querySelector('.contentContainer__paragraph')) {
+        this[0].querySelector('.contentContainer__paragraph').classList.remove('hidden');
+      }
+    },
+    onLeave: function () {
+      if (this[0].querySelector('.contentContainer__paragraph')) {
+        this[0].querySelector('.contentContainer__paragraph').classList.add('hidden');
+      }
+    }
+  });
 
-	//methods
-	$.fn.fullpage.setAllowScrolling(true);
+  //methods
+  $.fn.fullpage.setAllowScrolling(true);
 })
